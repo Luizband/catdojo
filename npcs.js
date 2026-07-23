b1b387143207297a882e2461986aece8dece7bed
Luizband <band.luizfelipe@gmail.com>
2026-07-22 22:36:14 -0300

    Update npcs.js

--- npcs.js
+++ npcs.js
@@ -32,7 +32,7 @@ const claDosCaninos = [
     },
     { 
       // Tier 1. Faixa 20 (+133 pts = 143 pts total) -> O Teto do Clã 1
-      id: "cpu5", cla: "Clã dos Caninos", nome: "Don Lorenzo", foto: "imagens/cpu05.png", 
+      id: "cpu5", cla: "Clã dos Caninos", nome: "Mestre Akita", foto: "imagens/cpu05.png", 
       faixa: 20, slots: 6, 
       forca: 35, agilidade: 12, resistencia: 56, foco: 10, vigor: 30, 
       isBoss: true, custoRep: 150, moedasBoss: 1000, xpBoss: 500, repBoss: 50,
@@ -53,28 +53,28 @@ const claDaPresa = [
     },
     { 
       // Tier 3. Faixa 6 (+35 pts = 50 pts total)
-      id: "cpu7", cla: "Clã da Presa", nome: "Cameo", foto: "imagens/cpu07.png", 
+      id: "cpu7", cla: "Clã da Presa", nome: "Sander", foto: "imagens/cpu07.png", 
       faixa: 6, slots: 2, 
       forca: 10, agilidade: 20, resistencia: 4, foco: 11, vigor: 5,
       habilidadesEquipadas: ["Rabo de Arraia", "Meia-Lua"]
     },
     { 
       // Tier 3. Faixa 11 (+70 pts = 85 pts total)
-      id: "cpu8", cla: "Clã da Presa", nome: "Zeca Tocaia", foto: "imagens/cpu08.png", 
+      id: "cpu8", cla: "Clã da Presa", nome: "Mestre Tuta", foto: "imagens/cpu08.png", 
       faixa: 11, slots: 4, 
       forca: 18, agilidade: 34, resistencia: 8, foco: 17, vigor: 8,
       habilidadesEquipadas: ["Rabo de Arraia", "Meia-Lua", "Armada", "Axé"]
     },
     { 
       // Tier 3. Faixa 16 (+105 pts = 120 pts total)
-      id: "cpu9", cla: "Clã da Presa", nome: "Ouro Fino", foto: "imagens/cpu09.png", 
+      id: "cpu9", cla: "Clã da Presa", nome: "Cameo", foto: "imagens/cpu09.png", 
       faixa: 16, slots: 4, 
       forca: 25, agilidade: 47, resistencia: 12, foco: 24, vigor: 12,
       habilidadesEquipadas: ["Rabo de Arraia", "Meia-Lua", "Martelo", "Parafuso"]
     },
     { 
       // Tier 3. Faixa 20 (+133 pts = 148 pts total) -> O Teto do Clã 2
-      id: "cpu10", cla: "Clã da Presa", nome: "Mestre Sabota", foto: "imagens/cpu10.png", 
+      id: "cpu10", cla: "Clã da Presa", nome: "Don Dilo", foto: "imagens/cpu10.png", 
       faixa: 20, slots: 6, 
       forca: 30, agilidade: 57, resistencia: 16, foco: 31, vigor: 14, 
       isBoss: true, custoRep: 250, moedasBoss: 2000, xpBoss: 1200, repBoss: 100,
@@ -95,7 +95,7 @@ const claArchea = [
     },
     { 
       // Tier 4. Faixa 8 (+49 pts = 66 pts total)
-      id: "cpu12", cla: "Clã Archea", nome: "Talon", foto: "imagens/cpu12.png", 
+      id: "cpu12", cla: "Clã Archea", nome: "Rojo", foto: "imagens/cpu12.png", 
       faixa: 8, slots: 3, 
       forca: 10, agilidade: 8, resistencia: 18, foco: 20, vigor: 10,
       habilidadesEquipadas: ["Bofetada Base", "Queda Felina", "Chave de Pata"]
@@ -116,7 +116,7 @@ const claArchea = [
     },
     { 
       // Tier 4. Faixa 20 (+133 pts = 150 pts total) -> O Ponto Máximo do Jogo!
-      id: "cpu15", cla: "Clã Archea", nome: "Conde Zero", foto: "imagens/cpu15.png", 
+      id: "cpu15", cla: "Clã Archea", nome: "Conde Pena", foto: "imagens/cpu15.png", 
       faixa: 20, slots: 6, 
       forca: 25, agilidade: 20, resistencia: 42, foco: 45, vigor: 18, 
       isBoss: true, custoRep: 400, moedasBoss: 4000, xpBoss: 2500, repBoss: 200,
